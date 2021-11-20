f0e41b80c84f3e8e954b7ae201409bab42ab5a93
Tên người dùng <quynhcongrua@gmail.com>
2021-11-20 19:42:13 +0700

    jxju

---
+++ main.js
@@ -0,0 +1,8 @@
+function news(a){
+    document.getElementsByClassName('active')
+    
+    console.log(document.getElementsByClassName('active').style)
+    // pre_active.name = 'nonactive'
+    // pre_active.style.display = 'none'
+    document.getElementById('news_button').href ='./' + a +'.html'
+}
